bf6acfc3493d53083d1ef07333f3481ecfff425e
Mohammed NavasKhan <mohammednavaslearn@gmail.com>
2023-10-25 19:34:19 +0530

    changes on content

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,45 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Assalamu Alaikum I am Mohammed NavasKhan, Software Engineer. You can
-          contact me on twitter
+          Assalamu Alaikum, Everyone, I am a <b>Software Engineer.</b>
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          A highly motivated and ambitious self-taught web developer with a keen
+          eye for design and a passion for building dynamic, user-friendly web
+          applications.
         </p>
+        <p>
+          I am proficient in the MERN stack (MongoDB, Express.js, React.js, and
+          Node.js), React Native, Docker, Jenkins, Photoshop and constantly
+          seeking to improve my skills and stay ahead of the latest industry
+          trends 🚀.
+        </p>
+        <p>
+          I have a strong understanding of web development principles and
+          extensive experience with JavaScript, HTML, and CSS 💻
+        </p>
+        <p>
+          I am skilled in developing and deploying full-stack web applications
+          that are both visually appealing and highly responsive 🎨
+        </p>
+        <p>
+          I am a problem-solver at heart with a desire to create visually
+          stunning and intuitive designs.
+        </p>
+        <p>
+          Follow me for insightful coding tutorials, tips and tricks, and the
+          latest updates on my projects.
+        </p>
+        <div>
+          <Link href="https://github.com/Mohammed-NavasKhan" target="blank">
+            Github
+          </Link>
+          {/* <br />
+          <Link href="https://github.com/alekszemz"> Sample Profile</Link>
+          <br />
+          <Link href="/posts/first-post"> First Post</Link>
+          <br /> */}
+        </div>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
